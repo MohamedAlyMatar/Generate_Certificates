71c7e10a365521ec6d101fbcc32b717f6cd3005a
“Mohamed <mohamedalymatar2001@gmail.com>
2023-08-04 11:15:08 +0200

    removed the arabic font part

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fpdf import FPDF
 import os
-from arabic_reshaper import arabic_reshaper
-from bidi.algorithm import get_display
 
 # ################################################################################# global variables
 
@@ -9,9 +7,7 @@ from bidi.algorithm import get_display
 font_path = r"fonts\Alice-Regular.ttf"  # Replace with the correct file path
 font_name = "Alice"      # A custom name for the font, you can choose any name
 
-arabic_path = r"fonts\NotoKufiArabic-Regular.ttf" 
-arabic_font = "NotoKufiArabic"
-
+# Change these based on the use
 input_file = 'level2_names.txt'
 certificate_template = 'level_2.png'
 output_folder = "level_2"
@@ -22,20 +18,6 @@ ranks = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", 
 rank = 0
 
 # ################################################################################# functions
-
-# Function to check if a string contains Arabic characters
-def contains_arabic(text):
-    arabic_ranges = [
-        (0x0600, 0x06FF),  # Arabic
-        (0x0750, 0x077F),  # Arabic Supplement
-        (0x08A0, 0x08FF),  # Arabic Extended-A
-        (0xFB50, 0xFDFF),  # Arabic Presentation Forms-A
-        (0xFE70, 0xFEFF)   # Arabic Presentation Forms-B
-    ]
-    for char in text:
-        if any(start <= ord(char) <= end for start, end in arabic_ranges):
-            return True
-    return False
 
 
 def capitalize_words(text):
@@ -54,7 +36,7 @@ with open(input_file, 'r') as file:
 class Certificate(FPDF):
     def header(self):
         # self.set_font('Arial', '', 45)
-        self.add_font(arabic_font, '', arabic_path, uni=True)  # Set is_rtl to True for Arabic text
+        # self.add_font(arabic_font, '', arabic_path, uni=True)  # Set is_rtl to True for Arabic text
         self.add_font(font_name, '', font_path, uni=True)
         self.set_font(font_name, '', 45)
         self.cell(0, 25, 'Certificate', align='C')
@@ -68,34 +50,21 @@ for name in names:
     certificate.add_page()
     certificate.image(certificate_template, x=0, y=0, w=2000, h=1414)
 
-    # if we get an arabic word (but still not working)
-    if contains_arabic(name):
-        
-        certificate.add_font(arabic_font, '', arabic_path, uni=True)
-        certificate.set_font(arabic_font, '', 45)
-        certificate.set_text_color(55, 55, 55)
-        reshaped_text = arabic_reshaper.reshape(name)
-        arabic_text = get_display(reshaped_text)
-        name_width = certificate.get_string_width(arabic_text)
-        name_x = (certificate.w - name_width) / 2
-        certificate.text(name_x, 585, arabic_text)
+    certificate_text = capitalize_words(name)
 
-    else:
-        certificate_text = capitalize_words(name)
+    # set different font size for each name size
+    words = name.split()
+    if len(words) == 2:
+        certificate.set_font(font_name, '', 350)
+    if len(words) == 3:
+        certificate.set_font(font_name, '', 320)
+    if len(words) > 3:
+        certificate.set_font(font_name, '', 220)
 
-        # set different font size for each name size
-        words = name.split()
-        if len(words) == 2:
-            certificate.set_font(font_name, '', 350)
-        if len(words) == 3:
-            certificate.set_font(font_name, '', 320)
-        if len(words) > 3:
-            certificate.set_font(font_name, '', 220)
-
-        certificate.set_text_color(55, 55, 55)
-        name_width = certificate.get_string_width(certificate_text)
-        name_x = (certificate.w - name_width) / 2
-        certificate.text(name_x, 585, certificate_text)
+    certificate.set_text_color(55, 55, 55)
+    name_width = certificate.get_string_width(certificate_text)
+    name_x = (certificate.w - name_width) / 2
+    certificate.text(name_x, 585, certificate_text)
 
     # writing the rank
     certificate.set_font(font_name, '', 210)
